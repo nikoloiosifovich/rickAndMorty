4175dddc4b4de7f6c8be70cc6d336759e035f483
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-04-01 03:12:19 -0300

    implement: route to render data on views

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const base = require("./database/characters.json");
 
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-  return res.render("index.html");
+  return res.render("index.html", { base });
 });
 
 module.exports = routes;
