84e156b8502dd782e8cab7c6b2103d654da985c9
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-04-02 17:38:13 -0300

    implement: routes to cosume data by oficial api rickandmorty

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,30 @@
 const express = require("express");
-const base = require("./database/characters.json");
+const axios = require("axios");
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
+routes.get("/", async (req, res) => {
+  const base = await axios
+    .get("https://rickandmortyapi.com/api/character/")
+    .then(response => {
+      return response.data.results;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
   return res.render("index.html", { base });
 });
 
 module.exports = routes;
+
+// let seen = {};
+// let uniqChar = base.filter(function(item) {
+//   if (seen.hasOwnProperty(item.status)) {
+//     return false;
+//   } else {
+//     seen[item.status] = true;
+//     console.log(seen);
+//     return true;
+//   }
+// });
